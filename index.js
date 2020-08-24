5fecc2ddf245ce10b1ca8a687ae9d4a0212b8f3a
TreeKR <redwikitv@gmail.com>
2020-08-24 13:35:40 +0900

    Install for windows

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const request = require('request')
 let window
 const util = require('./util')
 const fs = require('fs')
+const os = require('os')
+const mcDirWindows = 'C:\\Users\\' + os.userInfo().username + '\\AppData\\Roaming\\.minecraft'
 
 app.whenReady().then(() => {
     window = new BrowserWindow({
@@ -65,9 +67,36 @@ function doInstall(tab, event) {
     util.downloadFile('http://localhost:53573/download_client/' + tab.replace('(추천)', ''), fs.createWriteStream('./MyClient.jar'), () => {
         console.log('DL1 complete')
         event.reply('downloaded1')
-        util.downloadFile('http://localhost:53573/download_json/' + tab.replace('(추천)', ''), fs.createWriteStream('./MyClient.json'), () => {
+        util.downloadFile('http://treerasp.gwcr.xyz:53573/download_json', fs.createWriteStream('./MyClient.json'), () => {
             console.log('DL2 complete')
             event.reply('downloaded2')
+            if(os.type() === 'Windows_NT') {
+                if(!fs.statSync(mcDirWindows)) {
+                    fs.mkdir(mcDirWindows, (err) => {}) 
+                }
+                if(!fs.statSync(mcDirWindows + '\\versions')) {
+                    fs.mkdir(mcDirWindows + '\\versions', (err) => {})
+                }
+                if(!fs.statSync(mcDirWindows + '\\versions\\MyClient')) {
+                    fs.mkdir(mcDirWindows + '\\versions\\MyClient', (err) => {console.error(err)})
+                }
+                fs.copyFile('./MyClient.jar', mcDirWindows + '\\versions\\MyClient\\MyClient.jar', (err) => {
+                    if(err) {
+                        console.error(err)
+                        dialog.showErrorBox('오류!', 'jar을 복사하는데 오류가 났습니다. 죄송합니다. 아래의 정보를 복사&붙여넣기(또는 캡쳐)해서 개발자에게 전송해주세요.\n' + err.stack)
+                        process.exit(1)
+                    }
+                    fs.copyFile('./MyClient.json', mcDirWindows + '\\versions\\MyClient\\MyClient.json', (err) => {
+                        if(err) {
+                            console.error(err)
+                            dialog.showErrorBox('오류!', 'JSON을 복사하는데 오류가 났습니다. 죄송합니다. 아래의 정보를 복사&붙여넣기(또는 캡쳐)해서 개발자에게 전송해주세요.\n' + err.stack)
+                            process.exit(1)
+                        }
+                        dialog.showMessageBoxSync(window, { message: '설치가 완료되었습니다. 런처에서 release MyClient를 프로파일에 추가해 플레이하실수 있습니다.' })
+                    })
+                })
+                
+            }
         })
     })
 }
